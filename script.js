3953117d8f428b4604bd7fb376081b28eed7a3d0
csdesign-87 <71105749+csdesign-87@users.noreply.github.com>
2020-09-10 16:28:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var firstName = "Caleb";
+var lastName = "Salinas";
+alert("My name is " + firstName + " " + lastName);
+
+var age = 35
+alert("I am " + age + " years old.");
+
+if (age < 21) {
+		alert("It is illegal for me to drink in the states.");
+	}  else if (age > 21) {
+		alert("I am legally able to drink in the states.");
+	}  else {
+		alert("I am just barely able to drink legally in the states.");
+	}
+
+var coding = "I love this class.  I hope I pass with a B or higher.";
+console.log(coding);
