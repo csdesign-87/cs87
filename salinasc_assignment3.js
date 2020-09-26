b5fa42b8e0d2adb3ce7e4e54a86ad10d6ccc7fbc
csdesign-87 <71105749+csdesign-87@users.noreply.github.com>
2020-09-25 19:53:12 -0700

    Add files via upload

---
+++ salinasc_assignment3.js
@@ -0,0 +1,21 @@
+for(let i = 1000; i >= 0; i--) {
+	console.log(i);
+}
+
+for(let i = 0; i <= 10000; i++) { 
+	if (i = 2500) {
+	alert("quarterway");
+	} else if (i = 5000) {
+	alert("halfway");
+	} else if (i = 10000) {
+	alert("The loop is done!");
+	}
+
+const number = [1, 2, 3, 4, 5];
+const movies = ["super girl", "spartacus", "walking dead", "bumble bee", "olympus has fallen"]; 
+
+console.log("My # " + number[0] + "favorite tv show is " + movies[0]);
+console.log("My # " + number[1] + "favorite tv show is " + movies[1]);
+console.log("My # " + number[2] + "favorite tv show is " + movies[2]);
+console.log("My # " + number[3] + "favorite tv show is " + movies[3]);
+console.log("My # " + number[4] + "favorite tv show is " + movies[4]);
