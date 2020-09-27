ae3119b59b1d9141f2f500124260859f43981391
csdesign-87 <71105749+csdesign-87@users.noreply.github.com>
2020-09-26 18:04:24 -0700

    Add files via upload

--- salinasc_assignment3.js
+++ salinasc_assignment3.js
@@ -12,18 +12,23 @@ for(let i = 0; i <= 10000; i++) {
 	} else if (i = 10000) {
 	alert("The loop is done!");
 	}
+}
+
+const movies = ["super girl", "spartacus", "walking dead", "bumble bee", "olympus has fallen"];
 
 for(let i = 0; i <= 5; i++) {
 	if (i = 1) {
-	console.log("My #1 favorite tv show is Super Girl");
+	console.log("My #" + i + " favorite tv show is " + movies[0]);
 	} else if (i = 2) {
-	console.log("My #2 favorite tv show is Spartacus");
+	console.log("My #" + i + " favorite tv show is " + movies[1]);
 	} else if (i = 3) {
-	console.log("My #3 favorite tv show is Walking Dead");
+	console.log("My #" + i + " favorite tv show is " + movies[2]);
 	} else if (i = 4) {
-	console.log("My #4 favorite tv show is Bumble Bee");
+	console.log("My #" + i + " favorite tv show is " + movies[3]);
 	} else if (i = 5) {
-	console.log("My #5 favorite tv show is Olympus");
+	console.log("My #" + i + " favorite tv show is " + movies[4]);
 	}
+}
+
 	
 	
