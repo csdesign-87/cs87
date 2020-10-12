0ec45cd14be3060d98f53cedabdf055daf258389
csdesign-87 <71105749+csdesign-87@users.noreply.github.com>
2020-10-11 20:23:21 -0700

    Add files via upload

---
+++ salinasc_assessment.js
@@ -0,0 +1,13 @@
+function firstLetterName("Caleb") {
+	alert("The name " + firstLetterName + " starts with the letter C");
+}
+
+function divisibleByTwo(ParseInt(prompt("Choose a number")));
+for(i = 0; i < divisibleByTwo; i += 2);{
+divisibleByTwo.push(i);
+}
+if (i += 2) {
+alert("true");
+} else {
+alert("false);
+}
