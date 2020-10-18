3755d0f4e2e7b856339170212f26f554b28277ab
csdesign-87 <71105749+csdesign-87@users.noreply.github.com>
2020-10-17 20:38:59 -0700

    Add files via upload

---
+++ salinasc_assessment_dom.js
@@ -0,0 +1,9 @@
+const main = document.querySelector("main");
+
+var laugh1 = "What is the best thing about Switzerland, I don't know, but the flag is a big plus.";
+var laugh2 = "What lights up a soccer stadium, a soccer match.";
+var laugh3 = "What do you call a pig that does karate, a pork chop.";
+
+const template = documentquerySelector("My Jokes");
+
+const p = document.createElement("That's all folks");
